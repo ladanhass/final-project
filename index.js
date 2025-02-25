fba74c3ce6d3ce04e284a12c476625207cce6555
ladanhass <160683078+ladanhass@users.noreply.github.com>
2025-02-25 22:40:11 +0000

    changed journal to drop down

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 //port and initialise
 const app = express();
-const port = 8022;
+const port = 8050;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
