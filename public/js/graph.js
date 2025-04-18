0fb81541bb394b12ea57b62a717f42c84450748f
ladanhass <160683078+ladanhass@users.noreply.github.com>
2025-04-18 22:33:39 +0100

    improved graph design layout days appear in order

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,27 +1,61 @@
 document.addEventListener("DOMContentLoaded", function (){
 //fetches data 
 fetch("/journal/api/moods")
-.then(reponse => reponse.json())
+.then(response => response.json())
 .then(moods => {
 //extracts days and mood values from fetched data
-const labels = moods.map(mood => mood.day);
-const data = moods.map(mood => mood.mood);
+const weekDay =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const data = weekDay.map(day => {
+const moodEntry = moods.find(mood => mood.day === day);
+return moodEntry ? moodEntry.mood: 2;
+});
 const ctx = document.getElementById("moodGraph").getContext('2d');
+let gradient = ctx.createLinearGradient(500,0,0,0);
+gradient.addColorStop(0,'rgba(9,9,121)');
+gradient.addColorStop(1,'rgba(8,135,199 ,0.8)' );
 //creates a line graph
 new Chart(ctx, {
   type:'line',
   data:{
-  labels: labels,
+  labels: weekDay,
   datasets:[{
       data: data,
       label: "Mood",
-      borderColor:'rgb(75, 192, 192)',
-      fill: false,
-      tension: 0.1
+      borderColor:'rgba(35,30,64)',
+      backgroundColor: gradient,
+      fill: true,
+      tension: 0.2,
+      pointRadius: 4,
+      pointBackgroundColor: 'rgb(120, 124, 245)',
+      
+      
   }]
 },
   options: {
       responsive:true,
+      animation:{
+        duration:1300,
+        easing:'easeInSine',
+      },
+        plugins:{
+          tooltip:{
+            callbacks:{
+              label:function(context){
+                  const moodEmojis = {
+                    5: '😃 Happy',
+                    4: '🤩 Excited',
+                    3: '😐 Neutral',
+                    2: '😣 Angry',
+                    1: '😥 Sad'
+                  };
+                  const mood =context.raw;
+                  return moodEmojis[mood] || '';
+
+                }
+          }
+        }
+      },
+
       scales: {
           y:{
               beginAtZero:true,
@@ -38,23 +72,43 @@ new Chart(ctx, {
                     1: '😥 Sad'
                   };
                   return moodEmojis[value] || value;
-                }
+                },
+        
               },
               title:{
                 display:true,
-                text:'Mood'
-              }
+                text:'Mood',
+                font:{
+                  size:14,
+                  family:'Helvetica',
+                  weight: 'bold',
+                  
+              },
+              color:'rgb(120, 124, 245)'
+            }
 
           },
           x:{
             title:{
               display:true,
-              text: 'Days of the week'
+              text: 'Days of the week',
+              font:{
+                size:14,
+                family:'Helvetica',
+                weight: 'bold',  
+              },
+              color:'rgb(120, 124, 245)'
+            },
+            grid:{
+              display: false,
+
             }
           }
       }
   }
 });
 })
-//catch(console.error);
+.catch(error =>{
+  console.error("error mood", error);
+})
 });
